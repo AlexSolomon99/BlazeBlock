95d700711dd7d7d9ce89e842cc66e3235cb077ed
AlexSolomon99 <alexandru.solomon9999@gmail.com>
2023-10-08 03:28:06 +0300

    Added map stuff

--- src/webapp/map.py
+++ src/webapp/map.py
@@ -1,24 +1,27 @@
 from flask import Blueprint, render_template, request, flash, current_app, redirect, url_for, session, jsonify
 from .database_models import fire_data
 from . import db
+import json
 
 map = Blueprint('map', __name__)
-
-@map.route('/process_date_range', methods=['GET', 'POST'])
-def process_date_range():
-    if request.method == "POST":
-        date_range = request.form.get('date-range')
-        selected_map = request.form.get('select-map')
-
-        return
 
 
 @map.route('/map', methods = ['GET', 'POST'])
 def map_page():
     if request.method == 'POST':
-        if request.form.get('add_historical') == 'add_historical' :
-            list_of_ids = fire_data.FireDataUtils().download_historical_data(num_years_past=5.0)
-            
-            flash(f"Number of entries added to the db: {len(list_of_ids)}")
+        date_range = request.form.get('date-range')
+        selected_map = request.form.get('select-map')
 
-    return render_template("map.html")
+        date_1, date_2 = date_range.split(" - ")
+        date_1 = date_1.split("/")
+        date_2 = date_2.split("/")
+
+        if int(selected_map) == 1:
+            requested_data_dict = fire_data.FireDataUtils().return_individual_data(time_thr_1=f"{date_1[2]}-{date_1[0]}-{date_1[1]}", 
+                                                                                   time_thr_2=f"{date_2[2]}-{date_2[0]}-{date_2[1]}")
+        
+        elif int(selected_map) == 2:
+            requested_data_dict = fire_data.FireDataUtils().aggregate_data_over_time(time_thr_1=f"{date_1[2]}-{date_1[0]}-{date_1[1]}", 
+                                                                                     time_thr_2=f"{date_2[2]}-{date_2[0]}-{date_2[1]}")
+
+    return render_template("map.html", requested_data_dict=json.dumps(requested_data_dict))
